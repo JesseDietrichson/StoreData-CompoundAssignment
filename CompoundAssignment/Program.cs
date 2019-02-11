a482c44b6c77f9977b1e67a9af07c2492cd8f6e1
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-10 23:49:24 -0500

    Add project files.

---
+++ CompoundAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+
+            myInt += 10;
+
+            Console.WriteLine(myInt);
+
+        }
+    }
+}
